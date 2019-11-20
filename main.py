8427b967ab2b4266ef0e0665e00c7e694f288bdd
Victor <cizlota@gmail.com>
2019-11-20 15:45:04 +0300

    tweaks + macos standalone

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from os import mkdir, path
 import requests
 from tkinter import *
+from tkinter import filedialog
 
 bgc = '#FAEFEA'
 
@@ -22,9 +23,12 @@ getPhotos = BooleanVar()
 getVideos = BooleanVar()
 accessToken = StringVar()
 peerId = StringVar()
+offset = IntVar()
+folder = StringVar()
 
 def main():
-
+    TKroot.resizable(width = None, height = None)
+    TKroot.minsize(360, 270)
     TKroot.columnconfigure(0, weight=1)
     TKroot.rowconfigure(0, weight=1)
     root = Frame(TKroot, bg = bgc)
@@ -36,12 +40,15 @@ def main():
     root.rowconfigure(3, weight=1)
     root.rowconfigure(4, weight=1)
     root.rowconfigure(5, weight=1)
-    root.rowconfigure(6, weight=2)
+    root.rowconfigure(6, weight=1)
+    root.rowconfigure(7, weight=1)
+    root.rowconfigure(8, weight=2)
     root.columnconfigure(0, weight=1)
     root.columnconfigure(1, weight=5)
 
     Label(root, text="Access token:", bg = bgc).grid(row=0, sticky=E+W+S+N)
     Label(root, text="Peer id:", bg = bgc).grid(row=1, sticky=E+W+S+N)
+    Label(root, text="Offset:", bg = bgc).grid(row=2, sticky=E+W+S+N)
 
     tokenEntry = Entry(root, bg = bgc, textvariable=accessToken)
     tokenEntry.grid(row=0, column=1, sticky=E+W+S+N)
@@ -49,27 +56,35 @@ def main():
     peerEntry = Entry(root, bg = bgc, textvariable = peerId)
     peerEntry.grid(row=1, column=1, sticky=E+W+S+N)
     
+    offsetEntry = Entry(root, bg = bgc, textvariable = offset)
+    offsetEntry.grid(row=2, column=1, sticky=E+W+S+N)
+
     userChatButton = Checkbutton(root, text = 'User chat   ', bg = bgc, variable = userChat, anchor = W)
-    userChatButton.grid(row = 2, column = 1, sticky = E+W+S+N)
+    userChatButton.grid(row = 3, column = 1, sticky = E+W+S+N)
     
     membersOnlyButton = Checkbutton(root, text = 'Members only   ', bg = bgc, variable = membersOnly, anchor = W, justify = LEFT)
-    membersOnlyButton.grid(row = 3, column = 1, sticky = E+W+S+N)
+    membersOnlyButton.grid(row = 4, column = 1, sticky = E+W+S+N)
     
     getPhotosButton = Checkbutton(root, text = 'Photos   ', bg = bgc, variable = getPhotos, anchor = W, justify = LEFT)
-    getPhotosButton.grid(row = 4, column = 1, sticky = E+W+S+N)
+    getPhotosButton.grid(row = 5, column = 1, sticky = E+W+S+N)
     
     getVideosButton = Checkbutton(root, text = 'Videos   ', bg = bgc, variable = getVideos, anchor = W, justify = LEFT)
-    getVideosButton.grid(row = 5, column = 1, sticky = E+W+S+N)
+    getVideosButton.grid(row = 6, column = 1, sticky = E+W+S+N)
 
+    selectFolderButton = Button(root, text="Select folder", bg = bgc, command=selectDirectory)
+    selectFolderButton.grid(row = 7, column = 0, columnspan = 2, sticky = E+W+S+N)
 
     downloadButton = Button(root, text="Download", command=download, bg = bgc)
-    downloadButton.grid(row=6, column=0, columnspan = 3, sticky=E+W+S+N)
+    downloadButton.grid(row=8, column=0, columnspan = 2, sticky=E+W+S+N)
 
     TKroot.mainloop()
 
 def download():
     get(accessToken=accessToken.get(), peerId=peerId.get(), downloadPhotos=getPhotos.get(), downloadVideos=getVideos.get(), userChat=userChat.get(), membersOnly=membersOnly.get())
 
+def selectDirectory():
+    filename = filedialog.askdirectory()
+    folder.set(filename + '/')
 
 def get(accessToken = '', peerId = '', downloadPhotos = False, downloadVideos = False, userChat = False, membersOnly = False):
     api_version = '5.103'
@@ -89,12 +104,14 @@ def get(accessToken = '', peerId = '', downloadPhotos = False, downloadVideos = 
         if status == 200:
             member_list = response['response']['users']
             download_folder = response['response']['title']
+    download_folder = folder.get() + download_folder
     if not path.exists(download_folder):
         mkdir(download_folder)
     download_folder = download_folder + '/'
     if downloadPhotos:
         running = True
         start_from = ''
+        i = offset.get()
         while running:
             status, response = send_request(photos_url + peerId + start_from)
             if status == 200:
@@ -105,6 +122,9 @@ def get(accessToken = '', peerId = '', downloadPhotos = False, downloadVideos = 
                         id = [item['from_id']]
                         
                         if (userChat and ((not membersOnly) or (id == peerId))) or (not userChat and ((not membersOnly) or (set(id) & set(member_list) != set()))):
+                            if i > 0:
+                                i = i - 1
+                                continue
                             photo = item['attachment']['photo']['sizes']
                             photo_url = max(photo, key=lambda x:x['width'])['url']
                             date = item['attachment']['photo']['date']
@@ -126,6 +146,7 @@ def get(accessToken = '', peerId = '', downloadPhotos = False, downloadVideos = 
     if downloadVideos: 
         running = True
         start_from = ''
+        i = offset.get()
         while running:
             status, response = send_request(videos_url + peerId + start_from)
             if status == 200:
@@ -135,6 +156,9 @@ def get(accessToken = '', peerId = '', downloadPhotos = False, downloadVideos = 
                     for item in items:
                         id = [item['attachment']['video']['owner_id']]
                         if (userChat and ((not membersOnly) or (id == peerId))) or (not userChat and ((not membersOnly) or (set(id) & set(member_list) != set()))):
+                            if i > 0:
+                                i = i - 1
+                                continue
                             video = item['attachment']['video']['files']
                             if not ('mp4_240' in video):
                                 continue
